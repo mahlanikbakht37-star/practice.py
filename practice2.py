43e07e3ada7db940d4a93d913a2c1f67b79045b5
mahlanikbakht37-star <mahlanikbakht37@gmail.com>
2025-11-03 16:45:46 +0330

    Create practice2.py

---
+++ practice2.py
@@ -0,0 +1,15 @@
+# تابع برای میانگین نمرات
+def ave(score):
+  return sum(score)/ len (score)
+count=int(input("enter count number:"))  
+score = []
+for i in range (count):
+  score = float(input(enter your score:))
+  score.append(score)
+  average = ave#تابع
+if average>= 17: 
+  print("great")
+elif average<=12:
+  print("conditional!")
+else:
+  print("accept")
