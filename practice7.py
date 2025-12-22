fd2016759a1eaa82f7fe4f0933c9b793c178453c
mahla_nikbakht <mahlanikbakht37@gmail.com>
2025-12-22 22:38:51 +0330

    Create practice7.py

---
+++ practice7.py
@@ -0,0 +1,87 @@
+import csv
+class Contact:
+    def __init__(self, name, phone):
+        if phone.isdigit() == False:
+            raise ValueError("phone bayad adadi bashe")
+
+        self.name = name
+        self.phone = phone
+
+class PhoneBook:
+    def __init__(self):
+        self.list = []
+
+    def add_contact(self, name, phone):
+        c = Contact(name, phone)
+        self.list.append(c)
+
+    def show_all(self):
+        if len(self.list) == 0:
+            print("hich mokhatabi vojood nadare")
+            return
+
+        for c in self.list:
+            print(c.name, " -> ", c.phone)
+
+    def save_csv(self, file_name):
+        f = open(file_name, "w", newline="", encoding="utf-8")
+        writer = csv.writer(f)
+
+        writer.writerow(["name", "phone"])
+
+        for c in self.list:
+            writer.writerow([c.name, c.phone])
+
+        f.close()
+        print("zakhire shod")
+
+    def load_csv(self, file_name):
+        try:
+            f = open(file_name, "r", encoding="utf-8")
+            reader = csv.DictReader(f)
+            self.list = []
+
+            for row in reader:
+                try:
+                    c = Contact(row["name"], row["phone"])
+                    self.list.append(c)
+                except ValueError:
+                    pass   # phone ghalat bod
+
+            f.close()
+
+        except FileNotFoundError:
+            print("file peyda nashod")
+pb = PhoneBook()
+pb.load_csv("contacts.csv")
+
+while True:
+    print("\n1- ezafe kardan")
+    print("2- namayesh")
+    print("3- zakhire va exit")
+
+    try:
+        cmd = int(input("entekhab: "))
+    except ValueError:
+        print("adad vared kon")
+        continue
+
+    if cmd == 1:
+        n = input("name: ")
+        p = input("phone: ")
+
+        try:
+            pb.add_contact(n, p)
+            print("ok add shod")
+        except ValueError:
+            print("phone eshtebah ast")
+
+    elif cmd == 2:
+        pb.show_all()
+
+    elif cmd == 3:
+        pb.save_csv("contacts.csv")
+        break
+
+    else:
+        print("ghezine eshtebah")
